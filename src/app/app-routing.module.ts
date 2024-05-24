e26dd8c1861b9e4c97ac271d94729fdc943d7bfe
Sotonye0808 <sotydagz@gmail.com>
2024-05-24 16:07:09 +0100

    On branch main  Changes to be committed: 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
 
-
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'certificates', component: CertificatesComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },  // Default route
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-
-
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { CertificatesComponent } from './pages/certificates/certificates.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ThemeToggleComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CertificatesComponent
+  ],
   imports: [
-    CommonModule,
+    BrowserModule,
     AppRoutingModule
-  ]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
