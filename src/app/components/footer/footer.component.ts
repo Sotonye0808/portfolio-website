83f51f1bf37a40761fb65bc7fcd4a512e4323fed
Sotonye0808 <sotydagz@gmail.com>
2024-05-27 15:24:39 +0100

    On branch main  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/components/footer/footer.component.html 	modified:   src/app/components/footer/footer.component.ts 	modified:   src/app/components/navbar/navbar.component.html 	modified:   src/app/components/theme-toggle/theme-toggle.component.html 	modified:   src/app/components/theme-toggle/theme-toggle.component.ts 	modified:   src/app/pages/about/about.component.html 	modified:   src/app/pages/certificates/certificates.component.html 	modified:   src/app/pages/projects/projects.component.html 	modified:   src/styles.scss

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faGithub = faGithub;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+  faTwitter = faTwitter;
+  faEnvelope = faEnvelope;
 }
--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,20 +8,64 @@ import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './theme-toggle.component.html',
   imports: [FontAwesomeModule]
 })
-export class ThemeToggleComponent {
+export class ThemeToggleComponent implements OnInit {
   darkMode = false;
   faIcon = faMoon;
   faMoon = faMoon;
   faSun = faSun;
 
+  ngOnInit() {
+    if (this.isLocalStorageAvailable()) {
+      this.checkTheme();
+    }
+  }
+
+  isLocalStorageAvailable(): boolean {
+    try {
+      const test = '__localStorageTest__';
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  checkTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      this.darkMode = storedTheme === 'dark';
+      if (this.darkMode) {
+        document.documentElement.classList.add('dark');
+        this.faIcon = faSun;
+      } else {
+        document.documentElement.classList.remove('dark');
+        this.faIcon = faMoon;
+      }
+    } else {
+      // Default to system preference
+      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        this.darkMode = true;
+        document.documentElement.classList.add('dark');
+        this.faIcon = faSun;
+      }
+    }
+  }
+
   toggleTheme() {
     this.darkMode = !this.darkMode;
     if (this.darkMode) {
       document.documentElement.classList.add('dark');
-      this.faIcon = faSun;      
+      this.faIcon = faSun;
+      if (this.isLocalStorageAvailable()) {
+        localStorage.setItem('theme', 'dark');
+      }
     } else {
       document.documentElement.classList.remove('dark');
       this.faIcon = faMoon;
+      if (this.isLocalStorageAvailable()) {
+        localStorage.setItem('theme', 'light');
+      }
     }
   }
 }
