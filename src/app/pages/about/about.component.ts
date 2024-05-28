be52a084771f77936015bf6fec849623a533add5
Sotonye0808 <sotydagz@gmail.com>
2024-05-28 12:01:57 +0100

    On branch main  Changes to be committed: 	modified:   src/app/pages/about/about.component.html 	modified:   src/app/pages/about/about.component.scss 	modified:   src/app/pages/about/about.component.ts

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin, faXTwitter, faInstagram, faAngular, faReact, faHtml5, faCss3, faBootstrap, faNode, faPython, faJava, faGit, faSass } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faTwitter = faXTwitter;
+  faInstagram = faInstagram;
+  faEnvelope = faEnvelope;
+  faSass = faSass
+  faAngular = faAngular;
+  faReact = faReact;
+  faHtml5 = faHtml5;
+  faCss3 = faCss3;
+  faBootstrap = faBootstrap;
+  faNode = faNode;
+  faPython = faPython;
+  faJava = faJava;
+  faGit = faGit;
 }
