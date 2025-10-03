92ec2a358839dae3c11c590df2b69a95aaeb0710
Sotonye0808 <sotydagz@gmail.com>
2025-10-03 23:26:25 +0100

    styling adjustments to achieve glassy navbar and to the top button in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ContactLinks } from '../../pages/about/about.component';
 import { CommonModule } from '@angular/common';
+import { faChevronCircleUp } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -12,6 +13,11 @@ import { CommonModule } from '@angular/common';
 export class FooterComponent {
   
   year = new Date().getFullYear();
+  toTheTop = faChevronCircleUp;
 
   socialLinks = ContactLinks;
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
